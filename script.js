bfda42f02d25f6e83b4631017469f2e7acedb86c
Gio <giovanni.defrancesco@supermercato24.it>
2019-06-27 16:57:05 +0200

    refactor

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 const DEBUG = false;
 
 const breakfast = [
-    { name: "Latte vegetale con calcio aggiunto 200 ml + 1 yogurt greco 0% + 70 gr di pane ai cereali oppure 40 gr di fette biscottate di farro/kamut/cereali + 30 gr di frutta secca oleosa + 1 frutto fresco 150 gr", weight: 7 },
-    { name: "Chia pudding + latte vegetale con calcio aggiunto 200 ml + 40 gr di muesli oppure 40 gr di fette biscottate di farro / kamut / cereali + 1 frutto fresco 150 gr", weight: 7 },
-    { name: "Latte vegetale con calcio aggiunto 200 ml + 1 yogurt greco 0 % + torta allo yogurt 70 gr + 30 gr di frutta secca ammollata + 1 frutto fresco 150 gr", weight: 7 },
+    { name: "200ml Latte vegetale + 1 yogurt greco 0% + 70 gr di pane ai cereali oppure 40 gr di fette biscottate di farro/kamut/cereali + 30 gr di frutta secca oleosa + 1 frutto fresco 150 gr", weight: 7 },
+    { name: "100gr Chia pudding+ 200ml latte vegetale+ 40 gr di muesli oppure 40 gr di fette biscottate di farro / kamut / cereali + 1 frutto fresco 150 gr", weight: 7 },
+    { name: "200ml Latte vegetale + 1 yogurt greco 0 % + torta allo yogurt 70 gr + 30 gr di frutta secca ammollata + 1 frutto fresco 150 gr", weight: 7 },
     { name: "150 ml di latte vegetale con calcio aggiunto + pancake + 3 biscotti biologici + 30 gr di frutta secca + frutta secca 30 gr + 1 frutto fresco 150 gr", weight: 7 },
-    { name: "Latte vegetale con calcio aggiunto 200 ml + frittata di 1 uovo e 2 albumi(70 gr) + 40 gr di fette biscottate di farro / kamut / cereali o 70 gr di pane ai cereali + verdura facoltativa q.b. + 30 gr di frutta secca ammollata + 1 frutto fresco 150", weight: 2 },
-    { name: "Latte vegetale con calcio aggiunto 200 ml + asiago 50 gr + 40 gr di fette biscottate di farro / kamut / cereali o 70 gr di pane ai cereali + verdura facoltativa q.b. + 30 gr di frutta secca ammollata + 1 frutto fresco 150", weight: 1 },
-    { name: "Latte vegetale con calcio aggiunto 200 ml + hummus 200 gr + 30 gr di fette biscottate di farro / kamut / cereali o 50 gr di pane ai cereali + verdura facoltativa q.b. + 30 gr di frutta secca ammollata + 1 frutto fresco 150", weight: 7 }
+    { name: "200ml Latte vegetale + frittata di 1 uovo e 2 albumi(70 gr) + 40 gr di fette biscottate di farro / kamut / cereali o 70 gr di pane ai cereali + verdura facoltativa q.b. + 30 gr di frutta secca ammollata + 1 frutto fresco 150", weight: 2 },
+    { name: "200ml Latte vegetale + asiago 50 gr + 40 gr di fette biscottate di farro / kamut / cereali o 70 gr di pane ai cereali + verdura facoltativa q.b. + 30 gr di frutta secca ammollata + 1 frutto fresco 150", weight: 1 },
+    { name: "200ml Latte vegetale + hummus 200 gr + 30 gr di fette biscottate di farro / kamut / cereali o 50 gr di pane ai cereali + verdura facoltativa q.b. + 30 gr di frutta secca ammollata + 1 frutto fresco 150", weight: 7 }
 ];
 
 const mealBase = [
@@ -50,56 +50,50 @@ const dinnerProteins = [
     { name: "200 gr burger lupini", weight: 3 },
 ];
 
+var currentDiet = {};
+
 // since bootstrap will be bundle jQuery, I could use it but I don't want to...
 
 window.addEventListener('DOMContentLoaded', (event) => {
     // breakfast row
-    var trElement = document.createElement("tr");
-    var thElement = document.createElement("th");
-    thElement.textContent = "Colazione";
-    trElement.appendChild(thElement);
-    for (var i = 0; i < 6; i++) {
-        var tdElement = document.createElement("td");
-        var pElement = document.createElement("small");
-        pElement.textContent = arrRnd(breakfast);
-        trElement.appendChild(tdElement);
-        tdElement.appendChild(pElement);
-    }
-    document.getElementById('tableBody').appendChild(trElement);
+    generateRow(breakfast, "Colazione", (elem) => elem);
 
     // lunch row
-    trElement = document.createElement("tr");
-    thElement = document.createElement("th");
-    thElement.textContent = "Pranzo";
-    trElement.appendChild(thElement);
-    for (var i = 0; i < 6; i++) {
-        var tdElement = document.createElement("td");
-        var pElement = document.createElement("small");
-        pElement.textContent = mealBase[0] + ' + ' + lunchBase[0] + ' + ' + arrRnd(lunchProteins);
-        trElement.appendChild(tdElement);
-        tdElement.appendChild(pElement);
-    }
-    document.getElementById('tableBody').appendChild(trElement);
+    generateRow(lunchProteins, "Pranzo", (elem) => {
+        return mealBase[0] + ' + ' + lunchBase[0] + ' + ' + elem
+    });
 
     // dinner row
-    trElement = document.createElement("tr");
-    thElement = document.createElement("th");
-    thElement.textContent = "Cena";
-    trElement.appendChild(thElement);
-    for (var i = 0; i < 6; i++) {
-        var tdElement = document.createElement("td");
-        var pElement = document.createElement("small");
-        pElement.textContent = mealBase[0] + ' + ' + dinnerCereals[i % dinnerCereals.length] + ' + ' + arrRnd(dinnerProteins) + ' + ' + dinnerBase;
-        trElement.appendChild(tdElement);
-        tdElement.appendChild(pElement);
-    }
-    document.getElementById('tableBody').appendChild(trElement);
+    generateRow(dinnerProteins, "Cena", (elem) => {
+        return mealBase[0] + ' + ' + arrRnd(dinnerCereals) + ' + ' + elem + ' + ' + dinnerBase;
+    });
 
     //register all listeners
     document.getElementById('print').onclick = function () {
         window.print();
     }
 });
+
+var generateRow = function (arr, name, compositionRule) {
+    if (DEBUG) {
+        console.log(name + " phase");
+    }
+    currentDiet[name] = [];
+    var trElement = document.createElement("tr");
+    var thElement = document.createElement("th");
+    thElement.textContent = name;
+    trElement.appendChild(thElement);
+    for (var i = 0; i < 6; i++) {
+        var elem = compositionRule(arrRnd(arr));
+        currentDiet[name].push(elem);
+        var tdElement = document.createElement("td");
+        var pElement = document.createElement("small");
+        pElement.textContent = elem;
+        trElement.appendChild(tdElement);
+        tdElement.appendChild(pElement);
+    }
+    document.getElementById('tableBody').appendChild(trElement);
+}
 
 var arrRnd = function (arr) {
     const sumWeights = computeSumWeight(arr);
