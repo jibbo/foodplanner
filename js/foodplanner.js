9e8d06ad7cca7fa3f69341dec75e7f67a8697a01
Gio <giovanni.defrancesco@supermercato24.it>
2019-07-06 10:32:56 +0200

    fixed debug

--- js/foodplanner.js
+++ js/foodplanner.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const DEBUG = window.location.indexOf("localhost") > 0;
+const DEBUG = window.location.href.indexOf("localhost") > 0;
 
 window.addEventListener('DOMContentLoaded', (event) => {
     const db = new Db();
